b6380f55a70c845730b821d9712d42aae3399b79
Cesar <cesarday94@gmail.com>
2026-01-05 23:16:32 -0800

    first commit

--- record-collection-v2/src/main.js
+++
@@ -1,8 +0,0 @@
-import { createApp } from 'vue'
-import { createPinia } from 'pinia'
-import './style.css'
-import router from "./router/router.js"
-import App from './App.vue'
-
-
-createApp(App).use(router).use(createPinia()).mount('#app')
